7fd13404d5604792e9a6b5beaa1ac35b3fea1d84
Koushik Dutta <koushik.dutta.py@protonmail.com>
2024-09-12 01:35:32 +0530

    Update testdag.py

--- dags/testdag.py
+++ dags/testdag.py
@@ -1,30 +1,15 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
 from datetime import datetime
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.bash import BashOperator
 
-# Define a Python function to be run by the DAG
-def hello_world():
-    print("Hello, World!")
+with DAG(dag_id="medium_blog_dag", start_date=datetime(2024, 8, 8), schedule="0 0 * * *") as dag:
+    # Tasks are represented as operators
+    hello = BashOperator(task_id="hello", bash_command="echo hello")
 
-# Define default arguments for the DAG
-default_args = {
-    'start_date': datetime(2023, 1, 1),  # Adjust to your needs
-}
+    @task()
+    def airflow():
+        print("airflow")
 
-# Create the DAG
-with DAG(
-    'hello_world_dag',  # Name of the DAG
-    default_args=default_args,
-    schedule_interval=None,  # Set to None if you want to trigger manually
-    catchup=False,  # Skip backfilling for missed intervals
-) as dag:
-
-    # Define the task
-    hello_task = PythonOperator(
-        task_id='hello_world_task',  # Name of the task
-        python_callable=hello_world  # The Python function to call
-    )
-
-hello_task
-# If needed, add more tasks or dependencies
-# hello_task >> some_other_task
+    # Set dependencies between tasks
+    hello >> airflow()
